6dd08432ec8119cb35797529e62eb1d059f7f556
Mai Rodrigo <maialrodrigo@gmail.com>
2024-09-05 20:28:33 -0300

    expongo credenciales firebase por error vercel

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_APIKEY,
-	authDomain: import.meta.env.VITE_AUTHDOMAIN,
-	projectId: import.meta.env.VITE_PROJECTID,
-	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-	appId: import.meta.env.VITE_APPID,
+	apiKey: "AIzaSyAcRlKbbcChXLYkK7ODOsjALLEGslXO5bE",
+	authDomain: "cirfit-8a42c.firebaseapp.com",
+	projectId: "cirfit-8a42c",
+	storageBucket: "cirfit-8a42c.appspot.com",
+	messagingSenderId: "351282116690",
+	appId: "1:351282116690:web:3939a700ae550dc695478b",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore (app)
+export const db = getFirestore(app);
